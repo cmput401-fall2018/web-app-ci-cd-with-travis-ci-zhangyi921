7cccd62b431a4dc69f16a760f8fa23b75d41c563
Yi Zhang <yi16@ualberta.ca>
2018-11-04 15:28:45 -0700

    .

--- webresume/test_service.py
+++ webresume/test_service.py
@@ -1,12 +1,19 @@
 from service import Service
 from unittest.mock import patch
+import random
 # def test_bad_random():
 
-# def test_divide():
-@patch('Service.abs_plus')
-def test_abs_plus(MockClass1):
-	True
 
 # def test_complicated_function();
+@patch('x')
+def test_abs_plus(x):
+	x.return_value = 6
+	result = abs_plus(5)
+@patch('x')
+def test_complicated_function(x):
+	result = divide(5)
+@patch('x')
+def test_divide(x):
+	result = divide(5)
 
-test_abs_plus()
+	
